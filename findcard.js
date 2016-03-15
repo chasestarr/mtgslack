ad12cdb0dad957ac0b9f0e9077ee13ea188cf466
Chase Starr <chasestarr@gmail.com>
2016-03-15 00:52:28 -0400

    test

--- findcard.js
+++ findcard.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = function(req, res, next){
-    let text = req.body.text;
+    let textIn = req.body.text;
     let userName = req.body.user_name;
     // let usr = req.body.user_name;
     // let card = text.substr(text.indexOf(" ") + 1);
     var botPayload = {
-        text : userName
+        text : userName + " " + textIn
     };
 
     // avoid infinite loop
