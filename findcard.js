8869edf104f0879e90d874f989076c327e1b5ba2
Chase Starr <chasestarr@gmail.com>
2016-03-15 00:56:48 -0400

    test

--- findcard.js
+++ findcard.js
@@ -2,10 +2,19 @@
 module.exports = function(req, res, next){
     let textIn = req.body.text;
     let userName = req.body.user_name;
+    let textSplit = textIn.split(" ");
+    let textOut = "";
+    for(let i = 1; i < textSplit.length; i++){
+        if(i == textSplit.length - 1){
+            textOut += textSplit[i];
+        } else {
+            textOut += textSplit[i] + " ";
+        }
+    }
     // let usr = req.body.user_name;
     // let card = text.substr(text.indexOf(" ") + 1);
     var botPayload = {
-        text : userName + " " + textIn
+        text : textOut
     };
 
     // avoid infinite loop
