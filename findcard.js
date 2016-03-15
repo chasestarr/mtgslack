a3026cd93fb6a63f7218cbf3e249e0b4371ece9e
Chase Starr <chasestarr@gmail.com>
2016-03-15 00:42:13 -0400

    test

--- findcard.js
+++ findcard.js
@@ -3,7 +3,7 @@ module.exports = function(req, res, next){
     let text = req.body.text
     let card = text.substr(text.indexOf(" ") + 1);;
     var botPayload = {
-        text : card
+        text : "card"
     };
 
     // avoid infinite loop
