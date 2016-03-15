adbd8fc9ba5d4646142e57459ef6e67f884222ff
Chase Starr <chasestarr@gmail.com>
2016-03-15 00:38:48 -0400

    added strict to findcard

--- findcard.js
+++ findcard.js
@@ -1,8 +1,9 @@
+'use strict'
 module.exports = function(req, res, next){
     let text = req.body.text
     let card = text.substr(text.indexOf(" ") + 1);;
     var botPayload = {
-    text : card
+        text : card
     };
 
     // avoid infinite loop
