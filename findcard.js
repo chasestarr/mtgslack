5ba4184a24cc4600ab7c8c5712bb6ec6a9d701e7
Chase Starr <chasestarr@gmail.com>
2016-03-15 22:29:37 -0400

    add commands

--- findcard.js
+++ findcard.js
@@ -5,31 +5,34 @@ module.exports = function(req, res, next){
     let textIn = req.body.text;
     let userName = req.body.user_name;
     let textSplit = textIn.split(" ");
+    let cmd = textIn[1];
     let cardName = "";
-    for(let i = 1; i < textSplit.length; i++){
+    for(let i = 2; i < textSplit.length; i++){
         if(i == textSplit.length - 1){
             cardName += textSplit[i];
         } else {
             cardName += textSplit[i] + " ";
         }
     }
-    mtgapi(cardName, function(card){
-        var botPayload = {
-            "attachments": [
-                {
-                    "title": card.name,
-                    "title_link": card.url,
-                    "color": "good",
-                    "image_url": card.image_url
-                }
-            ]
-        };
+    if(cmd == "card"){
+        mtgapi(cardName, function(card){
+            var botPayload = {
+                "attachments": [
+                    {
+                        "title": card.name,
+                        "title_link": card.url,
+                        "color": "good",
+                        "image_url": card.image_url
+                    }
+                ]
+            };
 
-        // avoid infinite loop
-        if (userName !== 'slackbot') {
-        return res.status(200).json(botPayload);
-        } else {
-        return res.status(200).end();
-        }  
-    });  
+            // avoid infinite loop
+            if (userName !== 'slackbot') {
+            return res.status(200).json(botPayload);
+            } else {
+            return res.status(200).end();
+            }  
+        });   
+    } 
 }
